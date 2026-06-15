9022da0e581747efafdebfcccb8573488073156b
Lara Peddinghausen <larapedd@gmail.com>
2026-06-15 17:29:23 -0300

    feat(repository): adiciona metodos de busca por administrador e existencia de CPF

--- src/main/java/com/centraldeimoveis/api/repository/ImovelRepository.java
+++ src/main/java/com/centraldeimoveis/api/repository/ImovelRepository.java
@@ -1,9 +1,12 @@
 package com.centraldeimoveis.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.centraldeimoveis.api.model.imovel.Imovel;
 
 public interface ImovelRepository extends JpaRepository<Imovel, Integer>{
-    
+    // Busca apenas os imóveis onde o administrador dono tem o ID informado
+    Page<Imovel> findByAdministradorId(Long administradorId, Pageable pageable);
 }
--- src/main/java/com/centraldeimoveis/api/repository/ProprietarioRepository.java
+++ src/main/java/com/centraldeimoveis/api/repository/ProprietarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.centraldeimoveis.api.model.proprietario.Proprietario;
 
 public interface ProprietarioRepository extends JpaRepository<Proprietario, Integer> {
-    
+    boolean existsByCpf(String cpf);
 }
