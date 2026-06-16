db271c46b9b9ec029f2146a0d9436a8c6b3004cb
Lara Peddinghausen <larapedd@gmail.com>
2026-06-16 17:22:07 -0300

    feat(imovel): adiciona numero, complemento e tipo de locacao no DTO DadosAtualizaImovel

--- src/main/java/com/centraldeimoveis/api/dto/imovel/DadosAtualizaImovel.java
+++ src/main/java/com/centraldeimoveis/api/dto/imovel/DadosAtualizaImovel.java
@@ -1,10 +1,14 @@
 package com.centraldeimoveis.api.dto.imovel;
 
 import com.centraldeimoveis.api.model.imovel.Status;
+import com.centraldeimoveis.api.model.imovel.TipoLocacao;
 
 public record DadosAtualizaImovel(
     Integer id,
     String nome,
+    String numero,
+    String complemento,
+    TipoLocacao tipoLocacao,
     Status status, 
     Integer proprietario,
     String fotoUrl
