6c5916e31008366aa7da1c7c0c1271ff8c16ed92
Lara Peddinghausen <larapedd@gmail.com>
2026-06-26 20:58:59 -0300

    refactor(locacao): remove o campo dataTermino

--- src/main/java/com/centraldeimoveis/api/dto/locacao/DadosAtualizaLocacao.java
+++ src/main/java/com/centraldeimoveis/api/dto/locacao/DadosAtualizaLocacao.java
@@ -1,22 +1,18 @@
 package com.centraldeimoveis.api.dto.locacao;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 import com.centraldeimoveis.api.model.locacao.Status;
-import com.fasterxml.jackson.annotation.JsonFormat;
+
 
 public record DadosAtualizaLocacao(
     Integer id,
 
-    @JsonFormat(pattern = "dd/MM/yyyy")
-    LocalDate dataTermino,
-
-    BigDecimal aluguel,
-
     String observacao,
 
     Status status, 
+
+    BigDecimal aluguel,
 
     Integer pessoa
 ) {
